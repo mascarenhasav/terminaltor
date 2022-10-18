67fb8cac76ed3a470f843eb24f3cf92aafd9a768
mascarenhasav <mascarenhasav@gmail.com>
2022-10-18 21:16:54 +0900

    ae

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -1,20 +1,20 @@
 import config from '../../../config.json';
 
 const sumfetch = async (args: string[]): Promise<string> => {
-  if (config.ascii === 'mascarenhasa') {
+  if (config.ascii === 'mascarenhasav') {
     return `                                                
- sumfetch: summary display
+> sumfetch: summary display
 -----------
- ABOUT
- ${config.name}
-ﰩ ${config.ps1_hostname}
- <u><a href="${config.resume_url}" target="_blank">CV</a></u>
-爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+> ABOUT
+>> ${config.name}
+>> ${config.ps1_hostname}
+>> <u><a href="${config.resume_url}" target="_blank">CV</a></u>
+>> <u><a href="${config.repo}" target="_blank">Github repo</a></u>
 -----------
- CONTACT 
- <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
- <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
- <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+> CONTACT 
+>> <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+>> <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+>> <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
 -----------                                                                                                                                                                                                                   
 `;
   } 
